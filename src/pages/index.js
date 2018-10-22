2cb7e99eaebdbf27926257ba71b389f3fe02b4be
OlenaBL <35238049+OlenaBL@users.noreply.github.com>
2018-10-22 13:56:02 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>OMG! JAMstack! I'm in!</h1>
+    <p>Hello, I am Olena and I love coding. I can't wait to be part of JAMstack 2018 online hackathon!</p>
+  </Layout>
+)
+
+export default IndexPage
